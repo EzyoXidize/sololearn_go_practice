f113795dfcc0a63e9f11bd7a049ecd44327167a1
EzyoXidize <34023173+EzyoXidize@users.noreply.github.com>
2018-07-12 18:41:35 +0800

    Update lissajousPractice.go

--- lissajousPractice.go
+++ lissajousPractice.go
@@ -1,3 +1,4 @@
+// 产生随机的 lissajous gif
 package main
 
 import (
@@ -13,15 +14,15 @@ import (
 	"log"
 )
 
-var palette = []color.Color{color.White,color.Black}
+var palette = []color.Color{color.White,color.Black} // 色板,可改变参数让图形变成其他颜色
 
 const (
-	whiteIndex = 0
-	blackIndex = 1
+	whiteIndex = 0 // 画板中第一种,此处为白色
+	blackIndex = 1 // 第二种,此处为黑
 )
 
 func main()  {
-	rand.Seed(time.Now().UTC().UnixNano())
+	rand.Seed(time.Now().UTC().UnixNano())  // 获取随机种子
 	if len(os.Args) > 1 && os.Args[1] == "web" {
 		handler := func(w http.ResponseWriter, r *http.Request) {
 			lissajous(w)
@@ -37,13 +38,13 @@ func main()  {
 
 func lissajous(out io.Writer) {
 	const (
-		cycles = 5
-		res = 0.001
-		size = 100
-		nframes = 64
-		delay = 8
+		cycles = 5  // 完整的 x 振荡器变化的个数
+		res = 0.001	// 角度分辨率
+		size = 100	// 图像画布包含 [ -size..+size]
+		nframes = 64	// 帧数
+		delay = 8	// 帧间延迟
 	)
-	freq := rand.Float64() * 3.0
+	freq := rand.Float64() * 3.0	// y 振荡器的相对频率
 	anim := gif.GIF{LoopCount: nframes}
 	phase := 0.0
 	for i := 0; i < nframes; i++ {
